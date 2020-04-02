3fa6edc289576aff39e75072a49720ea3bc25cf7
DanielGiuggioli <62284516+DanielGiuggioli@users.noreply.github.com>
2020-04-02 11:51:57 +0200

    creazione primo progetto in cartella src

---
+++ src/Editing/Editing/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Editing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
