39498d45c88aa9db2da7b27522e997ce83c7f12c
DanielGiuggioli <62284516+DanielGiuggioli@users.noreply.github.com>
2020-05-14 15:55:51 +0200

    size

--- src/Editing/ImageEditing/Form1.Designer.cs
+++ src/Editing/ImageEditing/Form1.Designer.cs
@@ -52,23 +52,22 @@
             // 
             this.pictureBox1.BackColor = System.Drawing.Color.White;
             this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.pictureBox1.Location = new System.Drawing.Point(0, 114);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pictureBox1.Location = new System.Drawing.Point(0, 73);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(1071, 624);
+            this.pictureBox1.Size = new System.Drawing.Size(715, 400);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
             // menuStrip1
             // 
-            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
             this.menuStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.fileToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(1610, 40);
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(4, 1, 0, 1);
+            this.menuStrip1.Size = new System.Drawing.Size(1073, 26);
             this.menuStrip1.TabIndex = 1;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -78,28 +77,29 @@
             this.caricaImmagineToolStripMenuItem,
             this.salvaToolStripMenuItem});
             this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
-            this.fileToolStripMenuItem.Size = new System.Drawing.Size(72, 36);
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(44, 24);
             this.fileToolStripMenuItem.Text = "File";
             // 
             // caricaImmagineToolStripMenuItem
             // 
             this.caricaImmagineToolStripMenuItem.Name = "caricaImmagineToolStripMenuItem";
-            this.caricaImmagineToolStripMenuItem.Size = new System.Drawing.Size(213, 44);
+            this.caricaImmagineToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
             this.caricaImmagineToolStripMenuItem.Text = "Carica";
             this.caricaImmagineToolStripMenuItem.Click += new System.EventHandler(this.caricaImmagineToolStripMenuItem_Click);
             // 
             // salvaToolStripMenuItem
             // 
             this.salvaToolStripMenuItem.Name = "salvaToolStripMenuItem";
-            this.salvaToolStripMenuItem.Size = new System.Drawing.Size(213, 44);
+            this.salvaToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
             this.salvaToolStripMenuItem.Text = "Salva";
             // 
             // Matita
             // 
             this.Matita.Image = ((System.Drawing.Image)(resources.GetObject("Matita.Image")));
-            this.Matita.Location = new System.Drawing.Point(0, 42);
+            this.Matita.Location = new System.Drawing.Point(0, 27);
+            this.Matita.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Matita.Name = "Matita";
-            this.Matita.Size = new System.Drawing.Size(64, 64);
+            this.Matita.Size = new System.Drawing.Size(43, 41);
             this.Matita.TabIndex = 2;
             this.Matita.TabStop = false;
             this.Matita.Click += new System.EventHandler(this.pictureBox2_Click);
@@ -107,46 +107,50 @@
             // Pennarello
             // 
             this.Pennarello.BackColor = System.Drawing.Color.White;
-            this.Pennarello.Location = new System.Drawing.Point(70, 42);
+            this.Pennarello.Location = new System.Drawing.Point(47, 27);
+            this.Pennarello.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Pennarello.Name = "Pennarello";
-            this.Pennarello.Size = new System.Drawing.Size(64, 64);
+            this.Pennarello.Size = new System.Drawing.Size(43, 41);
             this.Pennarello.TabIndex = 3;
             this.Pennarello.TabStop = false;
             // 
             // Riempi
             // 
             this.Riempi.BackColor = System.Drawing.Color.White;
-            this.Riempi.Location = new System.Drawing.Point(140, 42);
+            this.Riempi.Location = new System.Drawing.Point(93, 27);
+            this.Riempi.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Riempi.Name = "Riempi";
-            this.Riempi.Size = new System.Drawing.Size(64, 64);
+            this.Riempi.Size = new System.Drawing.Size(43, 41);
             this.Riempi.TabIndex = 4;
             this.Riempi.TabStop = false;
             // 
             // Spray
             // 
             this.Spray.BackColor = System.Drawing.Color.White;
-            this.Spray.Location = new System.Drawing.Point(210, 42);
+            this.Spray.Location = new System.Drawing.Point(140, 27);
+            this.Spray.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Spray.Name = "Spray";
-            this.Spray.Size = new System.Drawing.Size(64, 64);
+            this.Spray.Size = new System.Drawing.Size(43, 41);
             this.Spray.TabIndex = 5;
             this.Spray.TabStop = false;
             // 
             // Ritaglia
             // 
             this.Ritaglia.BackColor = System.Drawing.Color.White;
-            this.Ritaglia.Location = new System.Drawing.Point(280, 42);
+            this.Ritaglia.Location = new System.Drawing.Point(187, 27);
+            this.Ritaglia.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Ritaglia.Name = "Ritaglia";
-            this.Ritaglia.Size = new System.Drawing.Size(64, 64);
+            this.Ritaglia.Size = new System.Drawing.Size(43, 41);
             this.Ritaglia.TabIndex = 6;
             this.Ritaglia.TabStop = false;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.BackColor = System.Drawing.SystemColors.ActiveBorder;
-            this.ClientSize = new System.Drawing.Size(1610, 1012);
+            this.ClientSize = new System.Drawing.Size(1073, 648);
             this.Controls.Add(this.Ritaglia);
             this.Controls.Add(this.Spray);
             this.Controls.Add(this.Riempi);
@@ -155,7 +159,6 @@
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
