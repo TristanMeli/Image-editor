c497108d53fe55527b63adfd87808f818c4a3535
alessiabirolinii <62284495+alessiabirolinii@users.noreply.github.com>
2020-05-17 20:00:38 +0200

    agg

--- src/Editing/ImageEditing/Form1.Designer.cs
+++ src/Editing/ImageEditing/Form1.Designer.cs
@@ -599,6 +599,7 @@
             this.button1.TabIndex = 24;
             this.button1.Text = "ruota";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // textBox1
             // 
--- src/Editing/ImageEditing/Form1.cs
+++ src/Editing/ImageEditing/Form1.cs
@@ -34,7 +34,7 @@ namespace ImageEditing
             //DrawArea = new Bitmap(pictureBox1.Size.Width, pictureBox1.Size.Height);
 
             //pictureBox1.Image = DrawArea;
-            Panel_Salva.Location = new System.Drawing.Point(371, 232)            //Graphics g;
+            Panel_Salva.Location = new System.Drawing.Point(371, 232);          //Graphics g;
             //g = Graphics.FromImage(DrawArea);
             //g.Clear(Color.Transparent);
             //g.Dispose();
@@ -528,11 +528,38 @@ namespace ImageEditing
             w = float.Parse(textBox4.Text);
 
             RectangleF A = new RectangleF(x, y, w, a);
-            GraphicsUnit h = GraphicsUnit.Pixel;
 
             System.Drawing.Image newImage = pictureBox1.Image;
-            newImage = ClassLibrary1.Class1.Taglia(x, y, a, w, newImage);
+            newImage = ClassLibrary2.Class1.Taglia(x, y, a, w, newImage);
             pictureBox1.Image = newImage;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!(radioButton1.Checked == true || radioButton2.Checked == true || radioButton3.Checked == true))
+                return;
+            var dlg = new OpenFileDialog();
+            var bitmap1 = Bitmap.FromFile(dlg.FileName);
+            pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
+            pictureBox1.Image = bitmap1;
+
+            if (radioButton1.Checked == true)
+            {
+                bitmap1.RotateFlip(RotateFlipType.Rotate90FlipX);
+                pictureBox1.Image = bitmap1;
+            }
+
+            if (radioButton2.Checked == true)
+            {
+                bitmap1.RotateFlip(RotateFlipType.Rotate180FlipX);
+                pictureBox1.Image = bitmap1;
+            }
+
+            if (radioButton3.Checked == true)
+            {
+                bitmap1.RotateFlip(RotateFlipType.Rotate270FlipX);
+                pictureBox1.Image = bitmap1;
+            }
         }
     }
 }
