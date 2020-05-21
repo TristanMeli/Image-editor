c849a9ddc0a83e986ee3f1cb0a43bc5637c8052d
walid <walidzaroual23@gmail.com>
2020-05-21 16:01:41 +0200

    icona
    icona

--- src/Editing/ImageEditing/Form1.Designer.cs
+++ src/Editing/ImageEditing/Form1.Designer.cs
@@ -1118,6 +1118,7 @@
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.menuStrip1);
             this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MainMenuStrip = this.menuStrip1;
             this.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.Name = "ImageEditor";
